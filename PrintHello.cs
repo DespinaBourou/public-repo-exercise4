c1c5ef46aa8b061455302b3c783bb5044011e483
DespinaBourou <despina.bourou@intragen.com>
2024-12-30 13:46:13 +0200

    first commit

---
+++ PrintHello.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testprojectex4
+{
+    public class PrintHello
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
